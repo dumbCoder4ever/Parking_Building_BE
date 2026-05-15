2966096726d77d38a0d43226bff82adfc1fc491c
ducky1103 <vuminhduc11032004@gmail.com>
2026-05-15 20:03:33 +0700

    add cors

--- src/main/java/fpt/swp391/parkingmanagement/config/SecurityConfig.java
+++ src/main/java/fpt/swp391/parkingmanagement/config/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(cors -> {})
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll() .requestMatchers(
                                 "/v3/api-docs/**",
