185ae31c35d6b4187de6cda0bba42c6b82b2aafc
ducky1103 <vuminhduc11032004@gmail.com>
2026-05-28 21:25:21 +0700

    fix java version and add author for swagger open api

--- src/main/java/fpt/swp391/parkingmanagement/config/OpenApiConfig.java
+++ src/main/java/fpt/swp391/parkingmanagement/config/OpenApiConfig.java
@@ -2,17 +2,39 @@ package fpt.swp391.parkingmanagement.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.beans.BeanProperty;
-
 @Configuration
 public class OpenApiConfig {
-   @Bean
-   public OpenAPI customOpenAPI() {
-return new OpenAPI().info(new Info().title("Parking Management").description("Parking Management API").version("1.0").description("Parking Management API Documentation").license(new License().name("Apache 2.0").url("http://springdoc.org")));
 
-   }
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        final String securitySchemeName = "bearerAuth";
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Parking Management")
+                        .version("1.0")
+                        .description("Parking Management API Documentation"))
+
+                // Add Security Requirement
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(securitySchemeName)
+                )
+
+                // Define JWT Bearer Scheme
+                .schemaRequirement(
+                        securitySchemeName,
+                        new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                );
+    }
 }
