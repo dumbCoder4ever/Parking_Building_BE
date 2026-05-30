c6263c9edd11e74d5dbef33a7ba495a992e7d95c
minigear12 <hero13456@gmail.com>
2026-05-30 20:45:12 +0700

    fix cors method patch

--- src/main/java/fpt/swp391/parkingmanagement/config/SecurityConfig.java
+++ src/main/java/fpt/swp391/parkingmanagement/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of("*")); // Allow specific origin
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
