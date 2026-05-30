5d85fe4743feaa611694104e264f037c5108f46a
minigear12 <hero13456@gmail.com>
2026-05-30 20:42:08 +0700

    CRUD of Users with cors config

--- src/main/java/fpt/swp391/parkingmanagement/service/AuthService.java
+++ src/main/java/fpt/swp391/parkingmanagement/service/AuthService.java
@@ -5,8 +5,11 @@ import fpt.swp391.parkingmanagement.dto.RegisterRequest;
 import fpt.swp391.parkingmanagement.entity.User;
 import fpt.swp391.parkingmanagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AuthService {
