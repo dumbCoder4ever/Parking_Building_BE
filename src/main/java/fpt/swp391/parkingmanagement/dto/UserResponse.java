3f40c2fc85964722e94a04949854b6280f6693cb
khanhphanHB <phbaokhanh357@gmail.com>
2026-06-06 23:15:46 +0700

    update usermanagement

--- src/main/java/fpt/swp391/parkingmanagement/dto/UserResponse.java
+++ src/main/java/fpt/swp391/parkingmanagement/dto/UserResponse.java
@@ -9,7 +9,7 @@ public class UserResponse {
     private String userId;
     private String username;
     private String fullName;
-    private String email;
+    private String gmail;
     private String phoneNumber;
     private String role;
     private String status;
