24e843d6c4ce031fa336557af25f084d62cfa1f9
minigear12 <hero13456@gmail.com>
2026-06-10 19:22:06 +0700

    Fix manager api mapping

--- src/main/java/fpt/swp391/parkingmanagement/controller/PricingPolicyController.java
+++ src/main/java/fpt/swp391/parkingmanagement/controller/PricingPolicyController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/pricing-policy")
+@RequestMapping("/api/manager/pricing-policy")
 @PreAuthorize("hasAnyRole('MANAGER','ADMIN')")
 public class PricingPolicyController {
 
